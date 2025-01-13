b7b7ca5906686ef299c55ece011a7d2a7db02029
DiggesT <Parchomencodan@mail.ru>
2025-01-13 21:01:50 +0700

    Create JSON endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 
+const members = [
+  { id: '1', name: 'Selezneva Elena', description: 'Mother' },
+  { id: '2', name: 'Parkhomenko Borris', description: 'Father' },
+]
+
 const expressApp = express()
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
+})
+
+expressApp.get('/list', (req, res) => {
+  res.send(members)
 })
 
 expressApp.listen(3000, () => {
