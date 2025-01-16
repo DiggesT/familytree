50aa133b918df46322e83a16abb491273c183d8b
DiggesT <Parchomencodan@mail.ru>
2025-01-16 21:27:29 +0700

    Fake data using lodash

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,15 +1,18 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
 
-const members = [
-  { id: '1', name: 'Selezneva Elena', description: 'Mother' },
-  { id: '2', name: 'Parkhomenko Borris', description: 'Father' },
-]
+const members = _.times(20, (i) => ({
+  id: i.toString(),
+  name: `Name ${i}`,
+  description: `Role ${i}`,
+  text: _.times(100, () => 'Some description text.').join(''),
+}))
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getMembers: trpc.procedure.query(() => {
-    return { members }
+    return { members: members.map((member) => _.pick(member, ['id', 'name', 'description'])) }
   }),
 })
 
