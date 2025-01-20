19b5c7858d57fee253f968b6c573e0df58b50678
DiggesT <Parchomencodan@mail.ru>
2025-01-20 19:25:22 +0700

    Form: error

--- webapp/src/pages/NewMemberPage/index.tsx
+++ webapp/src/pages/NewMemberPage/index.tsx
@@ -9,6 +9,7 @@ import { trpc } from '../../lib/trpc'
 
 export const NewMemberPage = () => {
   const [successMessageVisible, setSuccessMessageVisible] = useState(false)
+  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const createMember = trpc.createMember.useMutation()
 
   const formik = useFormik({
@@ -20,12 +21,19 @@ export const NewMemberPage = () => {
     },
     validate: withZodSchema(zCreateMemberTrpcInput),
     onSubmit: async (values) => {
-      await createMember.mutateAsync(values)
-      formik.resetForm()
-      setSuccessMessageVisible(true)
-      setTimeout(() => {
-        setSuccessMessageVisible(false)
-      }, 3000)
+      try {
+        await createMember.mutateAsync(values)
+        formik.resetForm()
+        setSuccessMessageVisible(true)
+        setTimeout(() => {
+          setSuccessMessageVisible(false)
+        }, 3000)
+      } catch (error: any) {
+        setSubmittingError(error.message)
+        setTimeout(() => {
+          setSubmittingError(null)
+        }, 3000)
+      }
     },
   })
 
@@ -42,6 +50,7 @@ export const NewMemberPage = () => {
         <Input name="role" label="Role" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
+        {!!submittingError && <div style={{ color: 'red' }}>{submittingError}</div>}
         {successMessageVisible && <div style={{ color: 'green' }}>Member created!</div>}
         <button type="submit" disabled={formik.isSubmitting}>
           {formik.isSubmitting ? 'Submitting...' : 'Create Member'}
