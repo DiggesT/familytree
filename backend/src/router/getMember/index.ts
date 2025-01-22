3027514d7ca4759e3e1324a442a6356b53e569bd
DiggesT <Parchomencodan@mail.ru>
2025-01-22 20:12:58 +0700

    Prisma: search member

--- backend/src/router/getMember/index.ts
+++ backend/src/router/getMember/index.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod'
-import { members } from '../../lib/members'
 import { trpc } from '../../lib/trpc'
 
-export const getMemberTrpcRoute = trpc.procedure.input(z.object({ id: z.string() })).query(({ input }) => {
-  const member = members.find((member) => member.id === input.id)
-  return { member: member || null }
+export const getMemberTrpcRoute = trpc.procedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
+  const member = await ctx.prisma.member.findUnique({
+    where: {
+      id: input.id,
+    },
+  })
+  return { member }
 })
