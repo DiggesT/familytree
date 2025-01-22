ba8ca3d1f6d98a99977071f5c83b9ee2561ecbe5
Anton Ryabov <antonryabovdev@gmail.com>
2025-01-22 19:47:59 +0300

    Fix: fix superjson

--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -1,6 +1,7 @@
 import type { TrpcRouter } from '@familytree/backend/src/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createTRPCReact, httpBatchLink } from '@trpc/react-query'
+import superjson from 'superjson'
 
 export const trpc = createTRPCReact<TrpcRouter>()
 
@@ -14,6 +15,7 @@ const queryClient = new QueryClient({
 })
 
 const trpcClient = trpc.createClient({
+  transformer: superjson,
   links: [
     httpBatchLink({
       url: 'http://localhost:3000/trpc',
