1a47fd84226d98c710c9ad7c8a81a481be10f6dc
DiggesT <Parchomencodan@mail.ru>
2025-01-23 19:26:39 +0700

    Auth: user model and registration endpoint

--- backend/src/router/createMember/input.ts
+++ backend/src/router/createMember/input.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 export const zCreateMemberTrpcInput = z.object({
-  lastName: z.string().min(1, 'Last Name is required'),
-  firstName: z.string().min(1, 'First Name is required'),
+  lastName: z.string().min(1, 'Last Name is required.'),
+  firstName: z.string().min(1, 'First Name is required.'),
   middleName: z.string(),
   text: z.string(),
 })
--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,6 +3,7 @@ import { trpc } from '../lib/trpc'
 import { createMemberTrpcRoute } from './createMember'
 import { getMemberTrpcRoute } from './getMember'
 import { getMembersTrpcRoute } from './getMembers'
+import { signUpTrpcRoute } from './signUp'
 // @endindex
 
 export const trpcRouter = trpc.router({
@@ -10,6 +11,7 @@ export const trpcRouter = trpc.router({
   createMember: createMemberTrpcRoute,
   getMember: getMemberTrpcRoute,
   getMembers: getMembersTrpcRoute,
+  signUp: signUpTrpcRoute,
   // @endindex
 })
 
