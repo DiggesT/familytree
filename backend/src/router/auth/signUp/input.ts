4cc53f67b325ab9ce6fb71031c2d7403400e677a
DiggesT <Parchomencodan@mail.ru>
2025-02-06 20:42:38 +0700

    Add emails for users

--- backend/src/router/auth/signUp/input.ts
+++ backend/src/router/auth/signUp/input.ts
@@ -5,5 +5,6 @@ export const zSignUpTrpcInput = z.object({
     .string()
     .min(1)
     .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and dashes.'),
+  email: z.string().min(1).email('Invalid email.'),
   password: z.string().min(1, 'Password is required.').max(8, 'Password should be 1-8 characters.'),
 })
--- webapp/src/pages/auth/SignUpPage/index.tsx
+++ webapp/src/pages/auth/SignUpPage/index.tsx
@@ -20,6 +20,7 @@ export const SignUpPage = withPageWrapper({
   const { formik, alertProps, buttonProps } = useForm({
     initialValues: {
       nick: '',
+      email: '',
       password: '',
       passwordAgain: '',
     },
@@ -52,6 +53,7 @@ export const SignUpPage = withPageWrapper({
       >
         <FormItems>
           <Input name="nick" label="Nick" formik={formik} />
+          <Input name="email" label="E-mail" formik={formik} />
           <Input name="password" label="Password" type="password" formik={formik} />
           <Input name="passwordAgain" label="Password Again" type="password" formik={formik} />
           <Alert {...alertProps} />
