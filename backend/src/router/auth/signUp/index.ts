50866eb9dfb0c340037148fdebfbe086ebe34268
DiggesT <Parchomencodan@mail.ru>
2025-02-07 20:23:07 +0700

    Email: send email function

--- backend/src/router/auth/signUp/index.ts
+++ backend/src/router/auth/signUp/index.ts
@@ -1,3 +1,4 @@
+import { sendWelcomeEmail } from '../../../lib/emails'
 import { trpc } from '../../../lib/trpc'
 import { getPasswordHash } from '../../../utils/getPasswordHash'
 import { signJWT } from '../../../utils/signJWT'
@@ -29,6 +30,8 @@ export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(a
       password: getPasswordHash(input.password),
     },
   })
+
+  void sendWelcomeEmail({ user })
 
   const token = signJWT(user.id)
 
