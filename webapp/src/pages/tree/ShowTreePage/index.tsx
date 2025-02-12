1d5bbc29f7050db8133d898bd3e2686b785606bc
DiggesT <Parchomencodan@mail.ru>
2025-02-12 17:12:32 +0700

    Draw tree and branches

--- webapp/src/pages/tree/ShowTreePage/index.tsx
+++ webapp/src/pages/tree/ShowTreePage/index.tsx
@@ -10,28 +10,66 @@ export const ShowTreePage = () => {
     return <></>
   }
 
-  const members = data.members.map((member) => {
-    return {
-      id: member.id,
-      mother: member.mother,
-      father: member.father,
-      name: `${member.lastName} ${member.firstName}`,
-    }
-  })
+  const members = data.members
+    .map((member) => {
+      return {
+        id: member.id,
+        mother: member.mother,
+        father: member.father,
+        name: `${member.lastName} ${member.firstName}`,
+      }
+    })
+    // TODO: remove this filter
+    .filter(
+      (member) =>
+        member.id !== '72a32a50-23ab-416a-b308-e68e5c2ff838' && member.id !== '1e8c979b-5275-4c7b-9ad8-d11126183590'
+    )
 
   const membersWithLevel = groupMembersByLevel(setMemberLevel(members[members.length - 1], members, 0), 0) // TODO: find current member
 
+  const maxLevel = membersWithLevel.length
+
   const tree = membersWithLevel.map((memberLevel, indexLevel) =>
     memberLevel.map((member, index) => (
-      <TreeMemberIcon key={member.id} name={member.name} y={indexLevel * 125} x={index * 150} />
+      <TreeMemberIcon
+        key={member.id}
+        name={member.name}
+        y={indexLevel * 150}
+        x={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index}
+      />
     ))
+  )
+
+  const branches = membersWithLevel.map((memberLevel, indexLevel) =>
+    memberLevel.map((_member, index) => {
+      if (indexLevel === 0) {
+        return <></> // TODO: skip branch for root member
+      }
+      return (
+        <line
+          x1={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index + 50}
+          y1={indexLevel * 150}
+          x2={
+            75 * (2 ** (maxLevel - indexLevel - 1) - 1) +
+            75 * 2 ** (maxLevel - indexLevel) * index +
+            50 +
+            75 * 2 ** (maxLevel - indexLevel - 1) * (index % 2 === 1 ? -1 : 1)
+          }
+          y2={indexLevel * 150 - 50}
+          stroke="black"
+          fill="transparent"
+          stroke-width="2"
+        />
+      )
+    })
   )
 
   return (
     <Segment title="Tree Name">
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="700" height="500">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="1000" height="550">
           {tree}
+          {branches}
         </svg>
       </div>
     </Segment>
