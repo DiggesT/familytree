ae67493057316c30fa7386ad191356a12cc36415
DiggesT <Parchomencodan@mail.ru>
2025-02-15 23:04:52 +0700

    Refactor show tree functions

---
+++ webapp/src/components/Trees/BinaryTree/index.tsx
@@ -0,0 +1,99 @@
+import { type TrpcRouterOutput } from '@familytree/backend/src/router'
+import { useState } from 'react'
+import { TreeMemberIcon } from '../SVG/TreeMemberIcon'
+import { groupMembersByLevel, setMemberLevel } from '../setMemberLevel/setMemberLevel'
+
+export const BinaryTree = ({ dataMembers }: { dataMembers: TrpcRouterOutput['getMembers']['members'] }) => {
+  const [currentMember, setCurrenMember] = useState<{ id: string; mother: string; father: string; name: string }>({
+    id: '',
+    mother: '',
+    father: '',
+    name: '',
+  })
+
+  const members = dataMembers.map((member) => {
+    return {
+      id: member.id,
+      mother: member.mother,
+      father: member.father,
+      name: `${member.lastName} ${member.firstName}`,
+    }
+  })
+
+  if (currentMember.id === '') {
+    setCurrenMember(members[0])
+  }
+
+  const memberOptions = dataMembers.map((member, index) => (
+    <option key={index} value={member.id}>
+      {member.lastName} {member.firstName} {member.middleName}
+    </option>
+  ))
+
+  const membersWithLevel = groupMembersByLevel(setMemberLevel(currentMember, members, 0), 0)
+
+  const maxLevel = membersWithLevel.length
+  const width = 100 + (2 ** (maxLevel - 1) - 1) * 150
+  const height = 100 + (maxLevel - 1) * 150
+
+  const tree = membersWithLevel.map((memberLevel, indexLevel) =>
+    memberLevel.map((member, index) => (
+      <TreeMemberIcon
+        key={member.id}
+        name={member.name}
+        y={indexLevel * 150}
+        x={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index}
+      />
+    ))
+  )
+
+  const branches = membersWithLevel.map((memberLevel, indexLevel) =>
+    memberLevel.map((member, index) => {
+      if (indexLevel === 0) {
+        return undefined // TODO: skip branch for root member
+      }
+      return (
+        <line
+          key={member.id}
+          x1={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index + 50}
+          y1={indexLevel * 150}
+          x2={
+            75 * (2 ** (maxLevel - indexLevel - 1) - 1) +
+            75 * 2 ** (maxLevel - indexLevel) * index +
+            50 +
+            75 * 2 ** (maxLevel - indexLevel - 1) * (index % 2 === 1 ? -1 : 1)
+          }
+          y2={indexLevel * 150 - 50}
+          stroke="black"
+          fill="transparent"
+          strokeWidth="2"
+        />
+      )
+    })
+  )
+
+  return (
+    <>
+      <div>
+        {/* TODO: add styles for select */}
+        <p>Select member:</p>
+        <select
+          defaultValue={currentMember.id}
+          onChange={(e) => {
+            e.preventDefault()
+            const selectedMember = members.find((member) => member.id === e.target.value)
+            selectedMember && setCurrenMember(selectedMember)
+          }}
+        >
+          {memberOptions}
+        </select>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width={width} height={height}>
+          {tree}
+          {branches}
+        </svg>
+      </div>
+    </>
+  )
+}
--- webapp/src/pages/tree/ShowTreePage/index.tsx
+++ webapp/src/pages/tree/ShowTreePage/index.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import { Alert } from '../../../components/Alert'
 import { Segment } from '../../../components/Segment'
-import { TreeMemberIcon } from '../../../components/Tree'
+import { BinaryTree } from '../../../components/Trees/BinaryTree'
 import { withPageWrapper } from '../../../lib/pageWrapper'
-import { setMemberLevel, groupMembersByLevel } from '../../../lib/setMemberLevel'
 import { trpc } from '../../../lib/trpc'
-import css from './index.module.scss'
 
 export const ShowTreePage = withPageWrapper({
   title: 'Show Tree',
@@ -12,102 +10,15 @@ export const ShowTreePage = withPageWrapper({
     me: getAuthorizedMe(),
   }),
 })(({ me }) => {
-  const [currentMember, setCurrenMember] = useState<{ id: string; mother: string; father: string; name: string }>({
-    id: '',
-    mother: '',
-    father: '',
-    name: '',
-  })
-
-  const { data } = trpc.getMembers.useQuery({ creator: me.id, limit: 100 })
-
-  if (!data?.members && data?.members.length !== 0) {
-    return <></>
-  }
-
-  const members = data.members.map((member) => {
-    return {
-      id: member.id,
-      mother: member.mother,
-      father: member.father,
-      name: `${member.lastName} ${member.firstName}`,
-    }
-  })
-
-  if (currentMember.id === '') {
-    setCurrenMember(members[0])
-  }
-
-  const memberOptions = data.members.map((member, index) => (
-    <option key={index} value={member.id}>
-      {member.lastName} {member.firstName} {member.middleName}
-    </option>
-  ))
-
-  const membersWithLevel = groupMembersByLevel(setMemberLevel(currentMember, members, 0), 0) // TODO: find current member
-
-  const maxLevel = membersWithLevel.length
-  const width = 100 + (2 ** (maxLevel - 1) - 1) * 150
-  const height = 100 + (maxLevel - 1) * 150
-
-  const tree = membersWithLevel.map((memberLevel, indexLevel) =>
-    memberLevel.map((member, index) => (
-      <TreeMemberIcon
-        key={member.id}
-        name={member.name}
-        y={indexLevel * 150}
-        x={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index}
-      />
-    ))
-  )
-
-  const branches = membersWithLevel.map((memberLevel, indexLevel) =>
-    memberLevel.map((member, index) => {
-      if (indexLevel === 0) {
-        return undefined // TODO: skip branch for root member
-      }
-      return (
-        <line
-          key={member.id}
-          x1={75 * (2 ** (maxLevel - indexLevel - 1) - 1) + 75 * 2 ** (maxLevel - indexLevel) * index + 50}
-          y1={indexLevel * 150}
-          x2={
-            75 * (2 ** (maxLevel - indexLevel - 1) - 1) +
-            75 * 2 ** (maxLevel - indexLevel) * index +
-            50 +
-            75 * 2 ** (maxLevel - indexLevel - 1) * (index % 2 === 1 ? -1 : 1)
-          }
-          y2={indexLevel * 150 - 50}
-          stroke="black"
-          fill="transparent"
-          strokeWidth="2"
-        />
-      )
-    })
-  )
+  const { data } = trpc.getMembers.useQuery({ creator: me.id, limit: 100 }) // TODO: unlimited
 
   return (
     <Segment title="Tree Name">
-      <div>
-        {/* TODO: add styles for select */}
-        <p>Select member:</p>
-        <select
-          defaultValue={currentMember.id}
-          onChange={(e) => {
-            e.preventDefault()
-            const selectedMember = members.find((member) => member.id === e.target.value)
-            selectedMember && setCurrenMember(selectedMember)
-          }}
-        >
-          {memberOptions}
-        </select>
-      </div>
-      <div className={css.tree}>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width={width} height={height}>
-          {tree}
-          {branches}
-        </svg>
-      </div>
+      {data && data.members.length > 0 ? ( // TODO: add more show tree variants
+        <BinaryTree dataMembers={data.members}></BinaryTree>
+      ) : (
+        <Alert color="brown">There are no members to create a tree.</Alert>
+      )}
     </Segment>
   )
 })
