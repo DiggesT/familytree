6d98e2a449e369153b2857e28c12161c4b8ef357
DiggesT <Parchomencodan@mail.ru>
2025-02-18 23:50:55 +0700

    Logging: express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,6 +31,15 @@ void (async () => {
 
     applyCron(ctx)
 
+    expressApp.use((error: unknown, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      logger.error('express', error)
+      if (res.headersSent) {
+        next(error)
+        return
+      }
+      res.status(500).send('Internal server error')
+    })
+
     expressApp.listen(env.PORT, () => {
       logger.info('express', `Listening at http://localhost:${env.PORT}`)
     })
